99f50aee821ed3547d644aadd29400b1ac047419
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-04-15 23:37:48 +0200

    feat: add URL path for retrieving individual shopping list details

--- apps/shopping_list/urls.py
+++ apps/shopping_list/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import apis
 urlpatterns = [
     path('shopping-lists/', apis.ShoppingListsListApi.as_view(), name='shopping-lists'),
+    path('shopping-lists/<int:shopping_list_id>/', apis.ShoppingListDetailApi.as_view(), name='shopping-list-detail'),
 ]
